7904c78d648f8aa3c73b282524a3c7d0cd740361
Vybornyi <1vybornyi@gmail.com>
2023-01-05 15:14:30 +0200

    refactored a code

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './styles.scss';
+import './styles/styles.scss';
 import App from './App';
 import store from './redux/store';
 
